adbbef4b41590f8554256eb24c2d4244ef4d04c4
Stephane Cusin <stephane.cusin@wayoos.com>
2014-10-15 00:20:19 +0200

    tar gz directory

--- src/crane/crane.go
+++ src/crane/crane.go
@@ -4,8 +4,12 @@ import (
 	"os"
 	"fmt"
 	"io"
+	"log"
 	"strconv"
+	"strings"
 	"net/http"
+	"archive/tar"
+	"compress/gzip"
 	"github.com/codegangsta/cli"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
@@ -57,6 +61,67 @@ func startServer(port int) {
 		m.Run()
 }
 
+func handleError( _e error ) {
+	if _e != nil {
+		log.Fatal( _e )
+	}
+}
+
+func TarGzWrite( _path string, tw *tar.Writer, fi os.FileInfo ) {
+	fr, err := os.Open( _path )
+	handleError( err )
+	defer fr.Close()
+
+	h := new( tar.Header )
+	h.Name = _path
+	//fi.Name()
+	h.Size = fi.Size()
+	h.Mode = int64( fi.Mode() )
+	h.ModTime = fi.ModTime()
+
+	err = tw.WriteHeader( h )
+	handleError( err )
+
+	_, err = io.Copy( tw, fr )
+	handleError( err )
+}
+
+func IterDirectory( dirPath string, tw *tar.Writer ) {
+	dir, err := os.Open( dirPath )
+	handleError( err )
+	defer dir.Close()
+	fis, err := dir.Readdir( 0 )
+	handleError( err )
+	for _, fi := range fis {
+		curPath := dirPath + "/" + fi.Name()
+		if fi.IsDir() {
+			//TarGzWrite( curPath, tw, fi )
+			IterDirectory( curPath, tw )
+		} else {
+			fmt.Printf( "adding... %s\n", curPath )
+			TarGzWrite( curPath, tw, fi )
+		}
+	}
+}
+
+func TarGz( outFilePath string, inPath string ) {
+	// file write
+	fw, err := os.Create( outFilePath )
+	handleError( err )
+	defer fw.Close()
+
+	// gzip write
+	gw := gzip.NewWriter( fw )
+	defer gw.Close()
+
+	// tar write
+	tw := tar.NewWriter( gw )
+	defer tw.Close()
+
+	IterDirectory( inPath, tw )
+
+	fmt.Println( "tar.gz ok" )
+}
 
 func main() {
 
@@ -94,6 +159,13 @@ func main() {
 			},
 			Action: func(c *cli.Context) {
 				println("Push task: ", c.Args().First())
+
+				// tar directory
+
+				targetFilePath := "test.tar.gz"
+				inputDirPath := "haproxy/"
+				TarGz( targetFilePath, strings.TrimRight( inputDirPath, "/" ) )
+
 			},
 		},
 		{
