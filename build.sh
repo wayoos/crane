74fc4babf6548ba95e7a83a86c8ef172862085a2
Stephane Cusin <stephane.cusin@wayoos.com>
2014-11-06 22:30:52 +0100

    remove src from github

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-go get github.com/codegangsta/cli
-go get github.com/go-martini/martini
-go get github.com/martini-contrib/render
-go get gopkg.in/yaml.v2
-go get github.com/jmcvetta/napping
+#go get github.com/codegangsta/cli
+#go get github.com/go-martini/martini
+#go get github.com/martini-contrib/render
+#go get gopkg.in/yaml.v2
+#go get github.com/jmcvetta/napping
 
 rm -rf pkg/darwin_amd64/wayoos.com
 rm -rf pkg/linux_amd64/wayoos.com
